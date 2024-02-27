14a3b7deb78b1164fe47a6654f4c334cc0ce5ada
Diego <diannewdev@gmail.com>
2024-02-27 13:29:03 -0400

    github commit

--- index.js
+++ index.js
@@ -3,12 +3,17 @@
 window.onscroll = function() {stickyFunction()};
 
 const mainDiv1 = document.getElementById('mainDiv1');
+const topMenu = document.getElementById('topMenu');
 const sticky = mainDiv1.offsetTop;
 
 function stickyFunction() {
   if (window.pageYOffset >= sticky) {
     mainDiv1.classList.add('mainDiv1-sticky');
+
+
   } else {
     mainDiv1.classList.remove('mainDiv1-sticky');
+
+
   }
 }
